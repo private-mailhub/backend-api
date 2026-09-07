c0b65c24fb3047676a121e705f108cb966a8bde7
Youngjin Mo <hello@private-mailhub.com>
2026-09-07 15:35:59 +0900

    Refactor: Move the backend to the repository root

--- back-end/src/common/enums/oauth-provider.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum OAuthProvider {
-  GITHUB = 'github',
-  APPLE = 'apple',
-  GOOGLE = 'google',
-}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import { createBaseConfig } from '../eslint.config.base.mjs';
+import { createBaseConfig } from './eslint.config.base.mjs';
 
 export default tseslint.config(
   { ignores: ['eslint.config.mjs'] },
